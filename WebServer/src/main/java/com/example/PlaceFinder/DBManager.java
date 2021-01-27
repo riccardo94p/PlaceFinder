5045b780cd4b8ca34446172c643e720098337d96
Leonardo Fontanelli <l.fontanelli2@studenti.unipi.it>
2021-01-27 18:35:00 +0100

    Implemented getNumReservations and various fixes

--- WebServer/src/main/java/com/example/PlaceFinder/DBManager.java
+++ WebServer/src/main/java/com/example/PlaceFinder/DBManager.java
@@ -2,7 +2,10 @@ package com.example.PlaceFinder;
 
 import com.example.PlaceFinder.entity.*;
 
+import java.math.BigInteger;
+import java.time.LocalDate;
 import java.util.*;
+import java.sql.Date;
 
 public interface DBManager {
 
@@ -10,12 +13,11 @@ public interface DBManager {
     void exit();
     boolean login(String username, String password);
     List<Reservation> browseUserReservations(String userId);
-    List<User> findCovidContact (String userId);
-    int updateCovidNotification(List<User> userList, boolean newNotification);
-    int notifyCovidContact(String userId);
+    boolean notifyCovidContact(String userId);
     boolean userReservation(String userid, int slotid, String roomid, Date date);
     boolean professorReservation(String userid, int slotid, String roomid, Date date);
     boolean deleteUserReservation(String userid, int slotid, String roomid, Date date);
     String addRoom(String idRoom, int numSeats, float capacity);
     boolean changeCapacity(String roomid, float capacity);
+    BigInteger getNumReservations(Date date, String room, int slot);
 }
